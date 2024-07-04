80e742e941895d4ea9aa225692e7d157731e34f0
SamridhG <samridh1998@gmail.com>
2024-07-04 22:37:07 +0530

    Profile Text Input Area Added

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,12 +1,28 @@
 import { useSelector } from "react-redux";
 import { signOutAuth } from "../config/firebase";
+import { useState } from "react";
 const Profile=()=>{
   const userName=useSelector((store)=>store.authSlice.currentUserName)
+  const [search,setSearch]=useState("")
     const onClick=()=>{
       signOutAuth()
     }
+    const onWriteNote=(value)=>{
+        setSearch(value)
+    }
     return (
-     <div><button onClick={onClick}className="bg-white text-tomatoOrange text-4xl">{userName}  LOGOUT</button></div>   
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="m-2 p-6 bg-white rounded-3xl shadow-red-800 shadow-2xl">
+           <div className="text-center mb-10 p-2 font-mono font-semibold text-gray-700 text-4xl">Hey {userName}</div>
+           <div className="flex">
+            <input className="p-3 w-[500px] h-[60px] text-4xl outline-none text-center font-mono" type="text" placeholder="Write Your Note Here" value={search} onChange={(e)=>{
+                   onWriteNote(e.target.value);
+           }}>
+           </input>
+           <button className="p-3 w-14 h-[60px] text-3xl text-center bg-tomatoOrange font-bold rounded-md">+</button>
+           </div>
+      </div>
+    </div>
     )
 } 
 export default Profile;
